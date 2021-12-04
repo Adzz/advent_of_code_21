ad59c7415a50f7f4dae3dec7f6599a6b3487f69e
Adam Lancaster <lancaster28@hotmail.com>
2021-12-04 16:36:33 +0000

    Day 1 problem statement

---
+++ lib/advent_of_code21.ex
@@ -0,0 +1,18 @@
+defmodule AdventOfCode21 do
+  @moduledoc """
+  Documentation for `AdventOfCode21`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AdventOfCode21.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
