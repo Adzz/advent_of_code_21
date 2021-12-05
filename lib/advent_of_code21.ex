ad5a1a1d2c4fba06f32e7eb6b199a29d7de099cc
Adam Lancaster <lancaster28@hotmail.com>
2021-12-05 14:11:04 +0000

    Day 1 part two
    Refectors to create a few "window" functions - essentially map and
    reduce that work on N elements of a collection at once.

--- lib/advent_of_code21.ex
+++ lib/advent_of_code21.ex
@@ -6,19 +6,57 @@ defmodule AdventOfCode21 do
   See README, essentially recur through the list and see how many times the measurement
   is more than its previous measurement.
   """
-  def day_1 do
-    AdventOfCode21.InputHelper.read!("day_1")
+  def day_1_part_1(data \\ AdventOfCode21.InputHelper.read!("day_1")) do
+    data
+    |> parse_as_ints()
+    |> count_increases()
+  end
+
+  def day_1_part_2(data \\ AdventOfCode21.InputHelper.read!("day_2")) do
+    data
+    |> parse_as_ints()
+    |> map_window(3, &Enum.sum/1)
+    |> count_increases()
+  end
+
+  defp parse_as_ints(data) do
+    data
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(&elem(Integer.parse(&1), 0))
-    |> scan_measurements(0)
   end
 
-  defp scan_measurements([], count), do: count
-  defp scan_measurements([_prev], count), do: count
+  @doc """
+  Counts the how many times the previous element is less than the next element in a list.
+  Iterates two at a time.
+  """
+  def count_increases(list) do
+    reduce_window(list, 2, 0, fn [prev, next], count ->
+      if prev < next, do: count + 1, else: count
+    end)
+  end
 
-  defp scan_measurements([prev, next | rest], count) do
-    new_count = if prev < next, do: count + 1, else: count
-    scan_measurements([next | rest], new_count)
+  @doc """
+  Maps through a list N at a time passing N elements to the mapping function and creating
+  a new list out of the results of those calls.
+  """
+  def map_window(list, window_size, mapper) when is_list(list) do
+    reduce_window(list, window_size, [], fn items, acc ->
+      [mapper.(items) | acc]
+    end)
+    |> :lists.reverse()
+  end
+
+  @doc """
+  Reduces over a list N at a time.
+  """
+  def reduce_window(list, window_size, acc, reducer) do
+    if length(list) < window_size do
+      acc
+    else
+      {tooken, rest} = Enum.split(list, window_size)
+      rest = Enum.drop(tooken, 1) ++ rest
+      reduce_window(rest, window_size, reducer.(tooken, acc), reducer)
+    end
   end
 end
--- test/advent_of_code21_test.exs
+++ test/advent_of_code21_test.exs
@@ -1,7 +1,28 @@
 defmodule AdventOfCode21Test do
   use ExUnit.Case
 
-  test "day 1" do
-    assert AdventOfCode21.day_1() == 1226
+  @day_1_test_data """
+  199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263
+  """
+
+  describe "day 1" do
+    test "part 1" do
+      assert AdventOfCode21.day_1_part_1() == 1226
+      assert AdventOfCode21.day_1_part_1(@day_1_test_data) == 7
+    end
+
+    test "part 2" do
+      assert AdventOfCode21.day_1_part_2(@day_1_test_data) == 5
+      assert AdventOfCode21.day_1_part_2() == 1252
+    end
   end
 end
