ad508b93b75c8f9ade9213ae77ff78d4b76ddf64
Adam Lancaster <lancaster28@hotmail.com>
2021-12-08 21:07:26 +0000

    Add day 4 p1 instructions

--- lib/day_4.ex
+++ lib/day_4.ex
@@ -1,2 +1,7 @@
 defmodule AdventOfCode21.Day4 do
+  def day_4_part_1(data \\ AdventOfCode21.InputHelper.read!("day_4_part_1")) do
+  end
+
+  def day_4_part_2(data \\ AdventOfCode21.InputHelper.read!("day_4_part_1")) do
+  end
 end
--- test/advent_of_code21_test.exs
+++ test/advent_of_code21_test.exs
@@ -38,6 +38,28 @@ defmodule AdventOfCode21Test do
   01010
   """
 
+  @day_4_test_data """
+  7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+  22 13 17 11  0
+   8  2 23  4 24
+  21  9 14 16  7
+   6 10  3 18  5
+   1 12 20 15 19
+
+   3 15  0  2 22
+   9 18 13 17  5
+  19  8  7 25 23
+  20 11 10 24  4
+  14 21 16 12  6
+
+  14 21 17 24  4
+  10 16 15  9 19
+  18  8 23 26 20
+  22 11 13  6  5
+   2  0 12  3  7
+  """
+
   describe "day 1" do
     test "part 1" do
       assert AdventOfCode21.Day1.day_1_part_1() == 1226
@@ -71,6 +93,18 @@ defmodule AdventOfCode21Test do
     test "part 2" do
       assert AdventOfCode21.Day3.day_3_part_2(@day_3_test_data) == 230
       assert AdventOfCode21.Day3.day_3_part_2() == 6_085_575
+    end
+  end
+
+  describe "day 4" do
+    test "part 1" do
+      assert AdventOfCode21.Day4.day_4_part_1(@day_4_test_data) == 198
+      # assert AdventOfCode21.Day4.day_4_part_1() == 2_250_414
+    end
+
+    test "part 2" do
+      # assert AdventOfCode21.Day4.day_4_part_2(@day_4_test_data) == 230
+      # assert AdventOfCode21.Day4.day_4_part_2() == 6_085_575
     end
   end
 end
