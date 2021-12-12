ad5411fd6cdc84907fca04d6110ea33c8274def0
Adam Lancaster <lancaster28@hotmail.com>
2021-12-12 23:52:00 +0000

    cleanup

--- lib/day_5.ex
+++ lib/day_5.ex
@@ -48,21 +48,22 @@ defmodule AdventOfCode21.Day5 do
 
   def expand_non_diagonal(_), do: :diagonal
 
+  # This could re-use all of part 1 but would break pt 1 tests so we repeat.
+
   def day_5_part_2(data \\ AdventOfCode21.InputHelper.read!("day_5")) do
     data
     |> String.split("\n", trim: true)
     |> Enum.reduce(%{}, fn coords, cells ->
-      coords
-      |> String.split(" -> ", trim: true)
-      |> Enum.map(fn xy ->
-        String.split(xy, ",", trim: true)
-        |> Enum.map(&String.to_integer/1)
-      end)
-      |> expand_line()
-      |> case do
-        :diagonal -> cells
-        line_cells -> Map.merge(cells, line_cells, fn _k, v1, _v2 -> v1 + 1 end)
-      end
+      line_cells =
+        coords
+        |> String.split(" -> ", trim: true)
+        |> Enum.map(fn xy ->
+          String.split(xy, ",", trim: true)
+          |> Enum.map(&String.to_integer/1)
+        end)
+        |> expand_line()
+
+      Map.merge(cells, line_cells, fn _k, v1, _v2 -> v1 + 1 end)
     end)
     |> Enum.reduce(0, fn {_key, v}, count -> if v >= 2, do: count + 1, else: count end)
   end
@@ -85,28 +86,28 @@ defmodule AdventOfCode21.Day5 do
     steps = max(y1, y2) - min(y1, y2)
 
     case {x1 < x2, y1 < y2} do
-      # RIGHT UP
+      # RIGHT & UP
       {true, true} ->
         0..steps
         |> Enum.reduce(%{}, fn step, acc ->
           Map.put(acc, {x1 + step, y1 + step}, 1)
         end)
 
-      # LEFT UP
+      # LEFT & UP
       {false, true} ->
         0..steps
         |> Enum.reduce(%{}, fn step, acc ->
           Map.put(acc, {x1 - step, y1 + step}, 1)
         end)
 
-      # RIGHT DOWN
+      # RIGHT & DOWN
       {true, false} ->
         0..steps
         |> Enum.reduce(%{}, fn step, acc ->
           Map.put(acc, {x1 + step, y1 - step}, 1)
         end)
 
-      # LEFT DOWN
+      # LEFT & DOWN
       {false, false} ->
         0..steps
         |> Enum.reduce(%{}, fn step, acc ->
